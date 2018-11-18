30aba2e17fc4c4802f34a6f9fcbd21ece9f8126b
Ayan Dasgupta <45137044+ayan4all@users.noreply.github.com>
2018-11-18 12:20:03 +0530

    Adding index file
    Adding the source code for index file, which will ask user to login.

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$errorMsg = "";
+$validUser = false;
+$_SESSION["login"] = false;
+if(isset($_POST["sub"])) {
+  $validUser = $_POST["username"] == "admin" && $_POST["password"] == "password";
+  if(!$validUser) $errorMsg = "Invalid username or password.";
+  else $_SESSION["login"] = true;
+}
+if($validUser) {
+   $_SESSION["username"] =  $_POST["username"];	
+   header("Location: ./home.php"); die();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <title>Login</title>
+</head>
+<body>
+  <form name="input" action="" method="post">
+    <p><label for="username">Username:</label><input type="text" value="<?php if(isset($_POST["username"])) echo $_POST["username"]; ?>" id="username" name="username" /></p>
+    <p><label for="password">Password:</label><input type="password" value="" id="password" name="password" /></p>
+    <div class="error"><?php echo $errorMsg; ?></div>
+    <input type="submit" value="Login" name="sub" />
+  </form>
+</body>
+</html>
